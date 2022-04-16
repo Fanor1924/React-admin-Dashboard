565704d1c8f777e8644e60b4be6413e4c8b82cac
fanor <fanorarmandochoque0@gmail.com>
2022-04-16 00:31:00 -0400

    sendfile

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
 const app = express();
 const port = process.env.PORT || 3000;
-
+const path = require('path');
 // sendFile ira aqui
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+  });
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
